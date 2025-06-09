52fbd167b05f122ed37afe29695d3f4ab2b7e79b
WeTi12 <235975@edu.p.lodz.pl>
2025-06-09 21:36:02 +0200

    added copy feature

--- script.js
+++ script.js
@@ -3,22 +3,45 @@
     document.getElementById("quote").style.visibility = "visible";
     const url = "https://gist.githubusercontent.com/WeTi12/61702558fca4580cba8d905333ad781d/raw/d7302967d334cbe3f27954b6a8df7d4641e6e12f/gistfile1.txt";
     try {
-      const response = await fetch(url);
-      if (!response.ok) throw new Error("Nie udało się wczytać pliku.");
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("Nie udało się wczytać pliku.");
 
-      const text = await response.text();
-      const lines = text.split('\n').map(line => line.trim()).filter(line => line !== '');
+        const text = await response.text();
+        const lines = text.split('\n').map(line => line.trim()).filter(line => line !== '');
 
-      const count = parseInt(lines[0], 10);
-      if (isNaN(count) || count < 1 || lines.length <= 1) {
-        document.getElementById('message').innerText = "Nieprawidłowy format pliku.";
-        return;
-      }
+        const count = parseInt(lines[0], 10);
+        if (isNaN(count) || count < 1 || lines.length <= 1) {
+            document.getElementById('message').innerText = "Nieprawidłowy format pliku.";
+            return;
+        }
 
-      const randomIndex = Math.floor(Math.random() * count) + 1;
-      const message = lines[randomIndex] || "(Brak wiadomości)";
-      document.getElementById('message').innerText = `${message}`;
+        const randomIndex = Math.floor(Math.random() * count) + 1;
+        const message = lines[randomIndex] || "(Brak wiadomości)";
+        document.getElementById('message').innerText = `${message}`;
     } catch (err) {
-      document.getElementById('message').innerText = "Błąd";
+        document.getElementById('message').innerText = "Błąd";
     }
-  }
+}
+
+function copyToClipboard() {
+    var copyText = ":adi: ";
+    var printText = '<span><img src="img/adison.png" alt="adi" class="image-adi">'
+    copyText += document.getElementById("message").innerHTML;
+    printText += document.getElementById("message").innerHTML;
+    printText += '</span>'
+
+    navigator.clipboard.writeText(copyText);
+
+    printText
+
+    document.getElementById("snackbar").innerHTML = "<div>Skopiowano: " + printText + "</div>";
+    showSnackbar();
+}
+
+function showSnackbar() {
+  var x = document.getElementById("snackbar");
+
+  x.className = "show";
+
+  setTimeout(function(){ x.className = x.className.replace("show", "hidden"); }, 3000);
+}
